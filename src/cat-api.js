1287da3c21d2e3d3293d01777e815a2f3560d414
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-27 16:41:59 +0300

    select options

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,24 @@
-
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+import { breedId } from './index';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const KEY = 'live_Hx8ktcm4HlPDrS1GRWIi3bNhI3TJEE42Jh3p70JuRDlHt9ZNF2P1iv3LNdA1a34e';
 
 export const fetchBreeds = () =>
-fetch(BASE_URL, {headers: {
-    'x-api-key': KEY
-}}).then((response) => {
+fetch(`${BASE_URL}/breeds?api_key=${KEY}`)
+.then((response) => {
     if(!response.ok) {
         throw new Error(response.status);
     }
     return response.json();     
 })
 
+
+export const fetchCatByBreed = () => 
+fetch(`${BASE_URL}/images/search?breed_id=${breedId}&api_key=${KEY}`)
+.then((response) => {
+    if(!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.blob(); 
+}).then((data) => {
+    console.log(data);
+})
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 import './css/style.css';
+export let breedId; 
+
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+// const KEY = 'live_Hx8ktcm4HlPDrS1GRWIi3bNhI3TJEE42Jh3p70JuRDlHt9ZNF2P1iv3LNdA1a34e';
 
 const selectBreeds = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loaderMassege = document.querySelector('.loader');
 const errorWarning = document.querySelector('.error');
 
+let storedBreeds = [];
+
 selectBreeds.classList.add('is-hidden');
 errorWarning.classList.add('is-hidden');
 loaderMassege.classList.add('is-hidden');
 
-// selectBreeds.addEventListener('click',(e) => {
-    
-//     const select = e.target;
-//     console.dir(select);
 
     fetchBreeds()
     .then((data) => {
@@ -34,20 +37,58 @@ loaderMassege.classList.add('is-hidden');
         //use the current array index
         option.value = i;
         option.innerHTML = `${breed.name}`;
+        option.id = `${breed.id}`;
         selectBreeds.appendChild(option);
+        // let breedId = breed.id;
         
+        // console.log(breedId);
         }
         //show the first breed by default
         // showBreedImage(0)
+       
         })
         .catch((err) =>
         console.log(err), 
         selectBreeds.classList.add('is-hiddden'),
     )
-// })
 
+    selectBreeds.addEventListener('click', onClick);
+    function onClick() {
+        
+     let breedId = null;
+        for (let option of selectBreeds.options)
+        {
+            if (option.selected) {
+                breedId = option.id;
+                
+            }
+        }
+        console.log(breedId);   
+        fetchCatByBreed();  
+    }
+    
 // function createMarkup(arr) {
 //     return arr.map((name) => `<p>${name}</p>`).join('');
     
 // }
 
+    // fetchCatByBreed()
+    // .then((data) => 
+    // console.log(data)
+    // )
+    // .catch((err) =>
+    // console.log(err)
+    // )
+
+    
+
+        // function showBreedImage(i)
+        // {
+        //     let catImage = document.createElement('img'); 
+        //     catImage.src= storedBreeds[i].image.url;
+          
+        //   catInfo.textContent= storedBreeds[i].temperament
+          
+        // }    
+    
+  
