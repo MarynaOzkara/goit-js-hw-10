937365567f9a39d43f232519d74a71e55c893305
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-28 10:31:39 +0300

    final fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 
 const selectBreeds = document.querySelector('.breed-select');
 const catInfoDiv = document.querySelector('.cat-info');
@@ -44,7 +45,7 @@ selectBreeds.addEventListener('change', () => {
     })
     .catch(error => {
       console.error('Error fetching cat by breed:', error);
-      showError();
+     showError();
     });
 });
 
@@ -58,10 +59,14 @@ function showCatInfo(catData) {
   const { breeds, url } = catData;
 
   const catInfoHTML = `
-    <img src="${url}" alt="${breeds[0].name}">
-    <h3>${breeds[0].name}</h3>
-    <p><strong>Description:</strong> ${breeds[0].description}</p>
-    <p><strong>Temperament:</strong> ${breeds[0].temperament}</p>
+    <div class="img-wrap">
+      <img src="${url}" alt="${breeds[0].name}">
+    </div>
+    <div class="info-wrap">
+      <h3>${breeds[0].name}</h3>
+      <p class="description"><strong>Description:</strong> ${breeds[0].description}</p>
+      <p class="breeds-temperament"><strong>Temperament:</strong> ${breeds[0].temperament}</p>
+    </div>
   `;
 
   catInfoDiv.innerHTML = catInfoHTML;
@@ -69,7 +74,8 @@ function showCatInfo(catData) {
 
 // Показати завантажувач
 function showLoader() {
-  loaderMassege.classList.remove('is-hidden');
+  // loaderMassege.classList.remove('is-hidden');
+  Notiflix.Notify.info('Loading data, please wait...');
 }
 
 // Приховати завантажувач
@@ -79,7 +85,8 @@ function hideLoader() {
 
 // Показати повідомлення про помилку
 function showError() {
-  errorWarning.classList.remove('is-hidden');
+  // errorWarning.classList.remove('is-hidden');
+  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
 }
 
     
